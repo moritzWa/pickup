655a0548c0e008c9587a35ce1d32c71dedfa490e
Andrew Duca <andrew.j.duca@gmail.com>
2024-08-02 17:21:07 -0700

    x

--- server/src/modules/content/infra/index.ts
+++ server/src/modules/content/infra/index.ts
@@ -1,6 +1,5 @@
 import {
     Content,
-    ContentMessage,
     ContentSession,
     Lesson,
     LessonProgress,
@@ -8,12 +7,8 @@ import {
 } from "src/core/infra/postgres/entities";
 import { PostgresContentRepository } from "./contentRepo";
 import { PostgresContentSessionRepository } from "./contentSessionRepo";
-import { PostgresContentMessageRepository } from "./contentMessageRepo";
 
 export const contentRepo = new PostgresContentRepository(Content);
 export const contentSessionRepo = new PostgresContentSessionRepository(
     ContentSession
-);
-export const contentMessageRepo = new PostgresContentMessageRepository(
-    ContentMessage
 );
