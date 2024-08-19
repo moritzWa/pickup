3447a2ca35991d3d1ea245820d2430e649f769ed
moritzWa <wallawitsch@gmail.com>
2024-08-19 15:08:52 -0700

    fix extractDomain

--- app/src/utils/author.ts
+++ app/src/utils/author.ts
@@ -2,15 +2,7 @@ export const extractDomain = (url: string) => {
   try {
     const hostname = new URL(url).hostname;
     const parts = hostname.split(".");
-    if (parts.length > 2) {
-      // Check for common subdomains
-      if (parts[0] === "www" || parts[0] === "blog") {
-        return parts[1];
-      }
-      // For other cases, join all parts except the last two
-      return parts.slice(0, -2).join(".");
-    }
-    return parts[parts.length - 2];
+    return parts.slice(-2, -1)[0];
   } catch {
     return url;
   }
