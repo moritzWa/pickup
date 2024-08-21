7314691469aaa39841f074c2b0ef08d5c5f59039
moritzWa <wallawitsch@gmail.com>
2024-08-20 18:42:37 -0700

    formatting

--- chrome-extension-mv3-firebase/src/popup/main-script.js
+++ chrome-extension-mv3-firebase/src/popup/main-script.js
@@ -1,26 +1,23 @@
-import { firebaseApp } from './firebase_config'
-import {
-    getAuth,
-    onAuthStateChanged
-} from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { firebaseApp } from "./firebase_config";
 // Auth instance for the current firebaseApp
 const auth = getAuth(firebaseApp);
 
-console.log("popup main!")
+console.log("popup main!");
 
-onAuthStateChanged(auth, user => {
-    if (user != null) {
-        console.log('logged in!');
-        console.log("current")
-        console.log(user)
-    } else {
-        console.log('No user');
-    }
+onAuthStateChanged(auth, (user) => {
+  if (user != null) {
+    console.log("logged in!");
+    console.log("current");
+    console.log(user);
+  } else {
+    console.log("No user");
+  }
 });
 
-document.querySelector('#sign_out').addEventListener('click', () => {
-    auth.signOut();
-    window.location.replace('./popup.html');
+document.querySelector("#sign_out").addEventListener("click", () => {
+  auth.signOut();
+  window.location.replace("./popup.html");
 });
 
 // Initialize button with user's preferred color
