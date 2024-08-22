80ca94583925da5a43c450186032848c10083e71
Andrew Duca <andrew.j.duca@gmail.com>
2024-08-21 23:38:12 -0700

    x

--- server/src/modules/content/services/audioGenerationQueue.ts
+++ server/src/modules/content/services/audioGenerationQueue.ts
@@ -1,38 +1,50 @@
 import { Queue, Worker } from "bullmq";
+import IORedis from "ioredis";
+import { config } from "src/config";
 import { contentRepo } from "src/modules/content/infra";
 import { AudioService } from "src/shared/audioService";
 import { Logger } from "src/utils";
 
-const audioGenerationQueue = new Queue("audioGeneration");
+const connection = new IORedis(config.redis.persistedRedisUrl);
 
-const audioGenerationWorker = new Worker("audioGeneration", async (job) => {
-    const { contentId, text, title } = job.data;
-
-    Logger.info(`Generating audio for contentId: ${title}`);
-
-    try {
-        const audioResponse = await AudioService.generate(text, title);
-        if (audioResponse.isFailure()) {
-            throw audioResponse.error;
-        }
-
-        const { url, lengthMs } = audioResponse.value;
-
-        // Update content with audio URL and length
-        await contentRepo.update(contentId, {
-            audioUrl: url,
-            lengthMs,
-        });
-
-        Logger.info(
-            `Audio generated and content updated for title: ${title}. Find audio at ${url}`
-        );
-    } catch (error) {
-        Logger.error(
-            `Failed to generate audio for contentId: ${contentId}`,
-            error
-        );
-    }
+const audioGenerationQueue = new Queue("audioGeneration", {
+    connection,
 });
+
+const audioGenerationWorker = new Worker(
+    "audioGeneration",
+    async (job) => {
+        const { contentId, text, title } = job.data;
+
+        Logger.info(`Generating audio for contentId: ${title}`);
+
+        try {
+            const audioResponse = await AudioService.generate(text, title);
+            if (audioResponse.isFailure()) {
+                throw audioResponse.error;
+            }
+
+            const { url, lengthMs } = audioResponse.value;
+
+            // Update content with audio URL and length
+            await contentRepo.update(contentId, {
+                audioUrl: url,
+                lengthMs,
+            });
+
+            Logger.info(
+                `Audio generated and content updated for title: ${title}. Find audio at ${url}`
+            );
+        } catch (error) {
+            Logger.error(
+                `Failed to generate audio for contentId: ${contentId}`,
+                error
+            );
+        }
+    },
+    {
+        connection,
+    }
+);
 
 export { audioGenerationQueue as AudioGenerationQueue };
