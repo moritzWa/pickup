6159e5ef35c8ecc174b73aae7b4496597b8869a8
moritzWa <wallawitsch@gmail.com>
2024-08-21 17:05:11 -0700

    add queue for audio processing

---
+++ server/src/modules/content/services/audioGenerationQueue.ts
@@ -0,0 +1,38 @@
+import { Queue, Worker } from "bullmq";
+import { contentRepo } from "src/modules/content/infra";
+import { AudioService } from "src/shared/audioService";
+import { Logger } from "src/utils";
+
+const audioGenerationQueue = new Queue("audioGeneration");
+
+const audioGenerationWorker = new Worker("audioGeneration", async (job) => {
+    const { contentId, text, title } = job.data;
+
+    Logger.info(`Generating audio for contentId: ${title}`);
+
+    try {
+        const audioResponse = await AudioService.generate(text, title);
+        if (audioResponse.isFailure()) {
+            throw audioResponse.error;
+        }
+
+        const { url, lengthMs } = audioResponse.value;
+
+        // Update content with audio URL and length
+        await contentRepo.update(contentId, {
+            audioUrl: url,
+            lengthMs,
+        });
+
+        Logger.info(
+            `Audio generated and content updated for title: ${title}. Find audio at ${url}`
+        );
+    } catch (error) {
+        Logger.error(
+            `Failed to generate audio for contentId: ${contentId}`,
+            error
+        );
+    }
+});
+
+export { audioGenerationQueue as AudioGenerationQueue };
